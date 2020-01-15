bcd6c6fb220d9ad09a31e6690189db6bf0c5f981
AdventureMahfuz <adventure.mahfuz@gmail.com>
2020-01-15 21:50:20 +0600

    beEasy eCommerce template

---
+++ assets/js/custom.js
@@ -0,0 +1,90 @@
+(function ($) {
+    $(document).ready(function () {
+        /*sticky header on scroll*/
+        $(window).on('scroll', function () {
+            var scroll = $(window).scrollTop();
+            if (scroll > 0) {
+                $("header.header-section").addClass("fixed-top");
+            } else {
+                $("header.header-section").removeClass("fixed-top");
+            }
+        });
+        //hero slider
+        $('.sliders').owlCarousel({
+            animateIn: 'fadeIn',
+            animateOut: 'fadeOut',
+            loop:true,
+            margin:0,
+            autoplay: true,
+            autoplayHoverPause:true,
+            responsiveClass:true,
+            nav:true,
+            dots:false,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:true
+                },
+                600:{
+                    items:1,
+                    nav:true
+                },
+                1000:{
+                    items:1,
+                    nav:true,
+                    loop:true
+                }
+            }
+        });
+        //slider animation
+        /*$('#slider').on('translate.owl.carousel',function () {
+            $('.item-content h1, .item-content p, .hero-cont-btn a, .hero-more-link a').removeClass('animated fadeInUp').css('opacity','0');
+            //$('.hero-cont-btn a').removeClass('animated slideInLeft').css('opacity','0');
+            //$('.hero-more-link a').removeClass('animated slideInRight').css('opacity','0');
+        });
+        $('#slider').on('translated.owl.carousel',function () {
+            $('.item-content h1, .item-content p, .hero-cont-btn a, .hero-more-link a').addClass('animated fadeInUp').css('opacity','0');
+            //$('.hero-cont-btn a').addClass('animated slideInLeft').css('opacity','1');
+            //$('.hero-more-link a').addClass('animated slideInRight').css('opacity','1');
+        });*/
+
+//materialize css activation
+        $('.tooltipped').tooltip();
+        $('.sidenav').sidenav();
+        /*$('.modal').modal();*/
+
+
+        //WoW js activation
+        //new WOW().init();
+
+        //responsive slick nav menu activation
+        /*$('#nav').slicknav({
+            prependTo:'.responsive-mobile-menu',
+        });*/
+
+        // magnific popup activation
+        /* $('.popup-img').magnificPopup({
+             gallery: {
+                 enabled: true
+             },
+             type: 'image'
+         });*/
+
+        //back to top show when scroll
+        /*$(".back_to_top i.fa").css("display", "none");
+        $(window).on('scroll', function () {
+            var scroll = $(window).scrollTop();
+            if (scroll < 300) {
+                $(".back_to_top i.fa").css("display", "none");
+            } else {
+                $(".back_to_top i.fa").css("display", "block");
+            }
+        });
+        $(".back_to_top i.fa").on("click",function () {
+            $("html").animate({
+                scrollTop: 0
+            }, 800);
+        });*/
+
+    });
+})(jQuery);
